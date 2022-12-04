1e3705a405463843392d87e7e6b12453fd7481e1
Alex Aguirre <teamaguirre@gmail.com>
2022-12-03 20:42:37 -0800

    fixed the return to index page path error

--- ballpy/reptile.py
+++ ballpy/reptile.py
@@ -1,20 +1,11 @@
-from flask import(Blueprint, render_template,request)
+from flask import(Blueprint, render_template,request,redirect)
 from . import models
 
 bp=Blueprint('reptile',__name__,url_prefix='/reptiles')
 
 # Get all of Reptiles in Json
-
-# Get a reptile by ID route
-
-# Get Add Reptile form route
-@bp.route('/new')
-def new_reptile():
-    return render_template('new.html')
-
-# Add Reptile to database route
-@bp.route('/',methods=["POST"])
-def Index_Add():
+@bp.route('/', methods=['GET','POST'])
+def Index():
     if request.method == "POST":
         newName= request.form['name']
         newType= request.form['reptile_type']
@@ -24,6 +15,18 @@ def Index_Add():
         models.db.session.add(newReptile)
         models.db.session.commit()
 
-        return render_template('index.html')
+        return redirect('../')
+
+    return "This returns the list of reptiles"
+# Get a reptile by ID route
+@bp.route('/<int:id>')
+def getReptile(id):
+    return "This returns a reptile by ID"
+
+
+# Get Add Reptile form route
+@bp.route('/new')
+def new_reptile():
+    return render_template('new.html')
 
 
